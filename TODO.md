e50437ca1a881bfa4dd00636c79e78c6f433ed57
r1ader <r1ader@qq.com>
2022-03-26 19:09:45 +0800

    simplify the code of example3

--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 # TODO
 
 ### 2022/3/26 add
-- [ ] simplify the code of example3
+- [x] simplify the code of example3
 - [x] update English document
 - [x] check if the code in stackblitz is available for long time
 - [ ] make a more awesome MotionGraphic and place it in the top of readme
--- code/example_3.vue
+++ code/example_3.vue
@@ -14,118 +14,99 @@ export default {
     const director = new R_director();
     director.take(this);
 
-    this.$refs.shadow
-        .r_animate({
-          opacity: 0,
-          transform: ' translateY(-50px) scale(0.1) scaleY([1~0.1])',
-          duration: 16,
-        })
-        .r_animate({
-          opacity: '[0~1]',
-          transform: 'translateY(-50px) scale([0.1~1]) scaleY(0.1)',
-          duration: 500,
-          interpolation: 'easeInQuad',
-        })
-        .r_animate({
-          opacity: '[0~1]',
-          transform: 'translateY(-50px) scale([0.1~1]) scaleY(0.1)',
-          duration: 300,
-          interpolation: 'easeOutQuad',
-          reverse: true,
-        })
-        .r_animate({
-          opacity: '[0~1]',
-          transform: 'translateY(-50px) scale([0.1~1]) scaleY(0.1)',
-          duration: 300,
-          interpolation: 'easeInQuad',
-        })
-        //
-        .r_animate({
-          opacity: '[0.5~1]',
-          transform: 'translateY(-50px) scale([0.3~1]) scaleY(0.1)',
-          duration: 200,
-          interpolation: 'easeOutQuad',
-          reverse: true,
-        })
-        .r_animate({
-          opacity: '[0.5~1]',
-          transform: 'translateY(-50px) scale([0.3~1]) scaleY(0.1)',
-          duration: 200,
-          interpolation: 'easeInQuad',
-        })
-        .r_animate({
-          opacity: '[0.7~1]',
-          transform: 'translateY(-50px) scale([0.7~1]) scaleY(0.1)',
-          duration: 100,
-          interpolation: 'easeOutQuad',
-          reverse: true,
-        })
-        .r_animate({
-          opacity: '[0.7~1]',
-          transform: 'translateY(-50px) scale([0.7~1]) scaleY(0.1)',
-          duration: 100,
-          interpolation: 'easeInQuad',
-        })
-        .r_animate({
-          opacity: '[0.9~1]',
-          transform: 'translateY(-50px) scale([0.9~1]) scaleY(0.1)',
-          duration: 50,
-          interpolation: 'easeOutQuad',
-          reverse: true,
-        })
-        .r_animate({
-          opacity: '[0.9~1]',
-          transform: 'translateY(-50px) scale([0.9~1]) scaleY(0.1)',
-          duration: 50,
-          interpolation: 'easeInQuad',
-        });
+    const down = (height) => {
+      return {
+        transform: `translateY([-${ height }~0]px)`,
+        duration: height * 2.5,
+        interpolation: 'easeInQuad',
+      };
+    };
+    const up = (height) => {
+      return {
+        ...down(height),
+        reverse: true,
+        interpolation: 'easeOutQuad',
+      };
+    };
+    let height_of_bounce = (times) => {
+      return 200 * Math.pow(3 / 4, times);
+    };
     this.$refs.circle
-        .r_animate({
-          transform: 'translateY([-200~0]px)',
-          duration: 500,
-          interpolation: 'easeInQuad',
-        })
-        .r_animate({
-          transform: 'translateY([0~-100]px)',
-          duration: 300,
-          interpolation: 'easeOutQuad',
-        })
-        .r_animate({
-          transform: 'translateY([-100~0]px)',
-          duration: 300,
-          interpolation: 'easeInQuad',
-        })
-        //
-        .r_animate({
-          transform: 'translateY([0~-50]px)',
-          duration: 200,
-          interpolation: 'easeOutQuad',
-        })
-        .r_animate({
-          transform: 'translateY([-50~0]px)',
-          duration: 200,
-          interpolation: 'easeInQuad',
-        })
-        .r_animate({
-          transform: 'translateY([0~-25]px)',
-          duration: 100,
-          interpolation: 'easeOutQuad',
-        })
-        .r_animate({
-          transform: 'translateY([-25~0]px)',
-          duration: 100,
-          interpolation: 'easeInQuad',
-        })
-        .r_animate({
-          transform: 'translateY([0~-12]px)',
-          duration: 50,
-          interpolation: 'easeOutQuad',
-        })
-        .r_animate({
-          transform: 'translateY([-12~0]px)',
-          duration: 50,
-          interpolation: 'easeInQuad',
-        });
+        .r_animate(down(height_of_bounce(0)))
+        // Although seems not elegant, but it's functional programing :)
+        // Maybe replace the follow code with `while` if you like
+        .r_animate(up(height_of_bounce(1)))
+        .r_animate(down(height_of_bounce(1)))
+        .r_animate(up(height_of_bounce(2)))
+        .r_animate(down(height_of_bounce(2)))
+        .r_animate(up(height_of_bounce(3)))
+        .r_animate(down(height_of_bounce(3)))
+        .r_animate(up(height_of_bounce(4)))
+        .r_animate(down(height_of_bounce(4)))
+        .r_animate(up(height_of_bounce(5)))
+        .r_animate(down(height_of_bounce(5)))
+        .r_animate(up(height_of_bounce(6)))
+        .r_animate(down(height_of_bounce(6)))
+        .r_animate(up(height_of_bounce(7)))
+        .r_animate(down(height_of_bounce(7)))
+        .r_animate(up(height_of_bounce(8)))
+        .r_animate(down(height_of_bounce(8)))
+        .r_animate(up(height_of_bounce(9)))
+        .r_animate(down(height_of_bounce(9)))
+        .r_animate(up(height_of_bounce(10)))
+        .r_animate(down(height_of_bounce(10)));
+
+    /*
+      let times = 1
+      while(times<=10){
+        this.$refs.circle
+          .r_animate(up(height_of_bounce(times)))
+          .r_animate(down(height_of_bounce(times)))
+        times++
+      }
+    */
+
+    const shadow_swell = (ratio) => {
+      return {
+        opacity: `[${ ratio }~1]`,
+        transform: `translateY(-50px) scale([${ ratio }~1]) scaleY(0.1)`,
+        duration: (1 - ratio) * 500,
+        interpolation: 'easeInQuad',
+      };
+    };
+
+    const shadow_Shrink = (ratio) => {
+      return {
+        ...shadow_swell(ratio),
+        reverse: true,
+        interpolation: 'easeOutQuad',
+      };
+    };
+    let ratio_of_shaow = (times) => {
+      return 1 - Math.pow(3 / 4, times);
+    };
+    this.$refs.shadow
+        .r_animate(shadow_swell(ratio_of_shaow(0)))
+        .r_animate(shadow_Shrink(ratio_of_shaow(1)))
+        .r_animate(shadow_swell(ratio_of_shaow(1)))
+        .r_animate(shadow_Shrink(ratio_of_shaow(2)))
+        .r_animate(shadow_swell(ratio_of_shaow(2)))
+        .r_animate(shadow_Shrink(ratio_of_shaow(3)))
+        .r_animate(shadow_swell(ratio_of_shaow(3)))
+        .r_animate(shadow_Shrink(ratio_of_shaow(4)))
+        .r_animate(shadow_swell(ratio_of_shaow(4)))
+        .r_animate(shadow_Shrink(ratio_of_shaow(5)))
+        .r_animate(shadow_swell(ratio_of_shaow(5)))
+        .r_animate(shadow_Shrink(ratio_of_shaow(6)))
+        .r_animate(shadow_swell(ratio_of_shaow(6)))
+        .r_animate(shadow_Shrink(ratio_of_shaow(7)))
+        .r_animate(shadow_swell(ratio_of_shaow(7)))
+        .r_animate(shadow_Shrink(ratio_of_shaow(8)))
+        .r_animate(shadow_swell(ratio_of_shaow(8)))
+        .r_animate(shadow_Shrink(ratio_of_shaow(9)))
+        .r_animate(shadow_swell(ratio_of_shaow(9)))
+        .r_animate(shadow_Shrink(ratio_of_shaow(10)))
+        .r_animate(shadow_swell(ratio_of_shaow(10)));
   },
 };
 </script>
